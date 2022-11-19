936b09aa43ce471d14da62718e6b1675a644e901
azibodusi osain <azibodusiosain@gmail.com>
2022-11-19 13:42:12 +0100

    add cicd with github actions

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Welcome to express app world");
+  res.send("Welcome to express app world. With CICD on Github actions");
 });
 
 app.get("/me", (req, res) => {
