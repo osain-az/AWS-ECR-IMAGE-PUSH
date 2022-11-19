4b70572566277411669c650abcaadad2d6ae2297
azibodusi osain <azibodusiosain@gmail.com>
2022-11-19 18:18:47 +0100

    update

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Welcome to express app world");
+  res.send("Welcome to express app world with CICD using github actions");
 });
 
 app.get("/me", (req, res) => {
